e60f49a8fdfd5334112dca65deeb7c7af5479081
Newton Maluvana <andzanimaluvana@gmail.com>
2025-03-26 17:12:16 +0100

    adding objects to the stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import ColorPellete from "./ColorPellete";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="text-red-700">Hello</div>
+      <div className="bg-blue-900   h-full grid-cols-4 relative">
+        <ColorPellete color={`bg-red-500 absolute top-0 left-0`} />
+        <ColorPellete color={`bg-red-500 absolute top-0 right-0 `} />
+        <ColorPellete color={`bg-red-500 absolute bottom-0 right-0 `} />
+        <ColorPellete color={`bg-yellow-500 absolute bottom-0 left-0`} />
+      </div>
     </>
   );
 }
---
+++ src/ColorPellete.jsx
@@ -0,0 +1,4 @@
+function ColorPellete(props) {
+  return <div className={`${props.color} h-[15%] w-[10%]`}>ColorPellete</div>;
+}
+export default ColorPellete;
